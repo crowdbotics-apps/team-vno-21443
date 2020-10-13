e572a29b501d525260bfe1085a9dd9d7e0a7a34d
Crowdbotics <team@crowdbotics.com>
2020-10-13 18:13:15 +0000

    Update models

--- backend/team_vno_21443/wsgi.py
+++ backend/team_vno_21443/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team_vno_21443.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team_vno_21443.settings")
 
 application = get_wsgi_application()
